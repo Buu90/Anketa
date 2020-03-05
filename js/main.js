9b4825ca91f5cc18f0880c42cdf0f149cea1a805
Buu90 <55487031+Buu90@users.noreply.github.com>
2020-03-06 00:03:24 +0300

    Add files via upload
    fix

--- js/main.js
+++ js/main.js
@@ -16,35 +16,62 @@ let flag = false;
           patronymic = prompt("Отчество", '');
               }	while (patronymic==flag||patronymic==null);
   
-  let age;
-          do {
-              age = parseInt(prompt("Возраст", ''));
-      } while (age==flag||age==null);
+              let point = true;
+              let age;
+              while(point){
+                
+                age = prompt("Возраст", '')
+            
+                if (!isNaN(+age)){
+                  point = false
+                }
+                if (age === ""){
+                  point = true
+                }
+              } 
   
-  let futureAge = (age +5);
+  
+  let futureAge = (+age + 5);
 
   let amountDays = (age * 365);
 
-    let male = confirm('Ваш пол мужской? Нажмите Ок если мужской, нажмите отмена если женский')
+    let male = confirm('Ваш пол мужской? Нажмите "Ок", если мужской, нажмите "Отмена", если женский')
+
 
     if (male == true) {
       male = "Мужской"
-    } else {
+    } else if (male == false) {
       male = "Женский"
     }
       let pension 
-    if (age<55) {
-      pension  = "Раздеватся и работать"
-    } else if  (age>100){
-      pension  = "Долгожитель"
+    if (age>=65 && male === "Мужской") {
+      pension  = "Да"
+    } else if  (age>=60 && male === "Женский"){
+      pension  = "Да"
     }
     else {
-      pension = "Пенсионер"
+      pension = "Нет"
     }
+
   alert(`
-      ваше ФИО: ${surname} ${name} ${patronymic} 
-      ваш возраст в годах: ${age}
-      ваш возраст в днях: ${amountDays}
-      через 5 лет вам будет: ${futureAge}
-      ваш пол: ${male}
-      вы на пенсии: ${pension}`);	
+      Ваше ФИО: ${surname} ${name} ${patronymic} 
+      Ваш возраст в годах: ${age}
+      Ваш возраст в днях: ${amountDays}
+      Через 5 лет вам будет: ${futureAge}
+      Ваш пол: ${male}
+      Вы на пенсии: ${pension}`);	
+
+      // function enter_pol() {
+      //   pol=confirm("Ваш пол - мужской?");
+      //   pol?pens=65:pens=60;
+      //   if(pens==65&&vozrast>=pens || pens==60&&vozrast>=pens) {
+      //     pens_confirm="да";
+      //   } else{
+      //     pens_confirm="нет"
+      //   }
+      //   if(pol){
+      //     pol="мужской";
+      //   } else{
+      //     pol="женский";
+      //   }
+      // }
