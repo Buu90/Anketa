4c2640111e6f89657079924deb901a9aff6b6b68
Buu90 <55487031+Buu90@users.noreply.github.com>
2020-03-06 17:30:19 +0300

    Add files via upload
    production

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ let flag = false;
                 if (!isNaN(+age)){
                   point = false
                 }
-                if (age === ""){
+                if (age === "" || age == null){
                   point = true
                 }
               } 
@@ -37,41 +37,18 @@ let flag = false;
 
     let male = confirm('Ваш пол мужской? Нажмите "Ок", если мужской, нажмите "Отмена", если женский')
 
-
-    if (male == true) {
-      male = "Мужской"
-    } else if (male == false) {
-      male = "Женский"
+    let pension
+    if (male) {
+      pension = (age >= 65) ? 'Да' : 'Нет';
+    } else {
+      pension = (age >= 60) ? 'Да' : 'Нет';
     }
-      let pension 
-    if (age>=65 && male === "Мужской") {
-      pension  = "Да"
-    } else if  (age>=60 && male === "Женский"){
-      pension  = "Да"
-    }
-    else {
-      pension = "Нет"
-    }
-
-  alert(`
+    let gender = (male) ? 'Мужской' : 'Женский';
+  
+    alert(`
       Ваше ФИО: ${surname} ${name} ${patronymic} 
       Ваш возраст в годах: ${age}
       Ваш возраст в днях: ${amountDays}
       Через 5 лет вам будет: ${futureAge}
-      Ваш пол: ${male}
+      Ваш пол: ${gender}
       Вы на пенсии: ${pension}`);	
-
-      // function enter_pol() {
-      //   pol=confirm("Ваш пол - мужской?");
-      //   pol?pens=65:pens=60;
-      //   if(pens==65&&vozrast>=pens || pens==60&&vozrast>=pens) {
-      //     pens_confirm="да";
-      //   } else{
-      //     pens_confirm="нет"
-      //   }
-      //   if(pol){
-      //     pol="мужской";
-      //   } else{
-      //     pol="женский";
-      //   }
-      // }
